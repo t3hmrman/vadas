1c8c657faffa7ac75d9404a5b2dd600ad8c91994
t3hmrman <t3hmrman@gmail.com>
2015-01-19 15:09:03 -0600

    Add proper import to namespace

--- src/vadas/core.clj
+++ src/vadas/core.clj
@@ -1,9 +1,10 @@
-(ns vadas.core)
+(ns vadas.core
+  (:import (edu.cmu.sphinx.api Configuration)))
 
 (defn start-listening
   "Test CMU Sphinx4 listener"
   []
-  (let [config (new edu.cmu.sphinx.api.Configuration)]
+  (let [config (new Configuration)]
     (println "Created new configuration: " (str config))))
 
 (defn -main
